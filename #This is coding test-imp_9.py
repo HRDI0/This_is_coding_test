1f93651f1399bd5c49bfbb52bc5e7a9201633039
HRDI0 <gjtjdals2546@gmail.com>
2021-09-10 03:50:55 +0900

    to solve imp problem

--- #This is coding test-imp_9.py
+++ #This is coding test-imp_9.py
@@ -37,15 +37,26 @@ from sys import stdin
 input = stdin.readline
 
 board = int(input())            #보드 크기
+board_list = [[0]*(board+2) for _ in range((board+2))]
 apple = int(input())            #사과 개수
 location = deque()
 for _ in range(apple):
     location.append(tuple(map(int,input().split())))
 
+while location:
+    ax,ay = location.popleft()
+    board_list[ax][ay] = 1
+
 num = int(input())      #방향 변환 횟수
 turn = deque()
 for _ in range(num):
-    turn.append(tuple(map(input().split())))      #n초 후 x 방향으로 이동 (L = 왼쪽, D = 오른쪽)
+    input_data = input().split()
+    turn.append((int(input_data[0]),input_data[1]))      #n초 후 x 방향으로 이동 (L = 왼쪽, D = 오른쪽)
+turn.append((1,'D'))
+# #중요!
+# 아래에 알고리즘에서는 action이라는 변수를 늘려가며 turn의 인덱스를 확인한다.
+# 만약 뱀이 죽지않고 turn의 시간보다 더 오래 살게 되면 action 변수가 turn의 인덱스를 넘게 되어
+# 인덱스 에러가 난다. 리스트나 큐같은 자료구조를 확인하는 인덱스 변수의 범위를 잘 정해놓아야 한다.
 
 # 방향 정보를 입력 받을 때, 시간 정보가 섞여서 들어 올수 있으니 시간 기준 정렬을 하는게 좋으나, 
 # 현재 문제에서는 시간순으로 입력되는 것으로 보여 정렬하지 않고 진행한다.
@@ -54,3 +65,55 @@ for _ in range(num):
 # 뱀의 초기 길이 = 1
 # 뱀의 초기 방향 X축 으로 +1만큼 이동 (행렬에서는 열을 따라 +1씩 이동.)
 
+dx = [0,1,0,-1]     #동남서북
+dy = [1,0,-1,0]
+direction = 0       #방향 전환을 위한 인덱스 번호
+time = 0            #진행하는 시간
+action = 0          #방향 전환할 데이터를 뽑는 플래그
+x,y = 1,1           #처음 위치
+board_list[x][y] = 2        #뱀 표시
+snake = deque()             #뱀 길이
+snake.append((x,y))
+while True:
+    t, dr = turn[action][0], turn[action][1]
+    if time == t:
+        action+=1
+        if dr == 'L':
+            direction = (direction-1)%4
+            nx = x + dx[direction]
+            ny = y + dy[direction]
+        elif dr == 'D':
+            direction = (direction+1)%4
+            nx = x + dx[direction]
+            ny = y + dy[direction]
+        if 1<=nx and nx<= board and 1<=ny and ny<= board and board_list[nx][ny] != 2:
+            snake.append((nx,ny))   #맵의 끝이 아니고 뱀 몸통이 아니라면 몸을 늘려 1칸 전진
+            if board_list[nx][ny] == 1:     #전진한 칸이 사과라면
+                board_list[nx][ny] = 2      #몸길이를 1칸 늘림
+            else:
+                board_list[nx][ny] = 2
+                px,py = snake.popleft()
+                board_list[px][py] = 0
+            x,y = nx,ny
+            time +=1
+        else:
+            time+=1
+            break
+    else:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+        if 1<=nx and nx<= board and 1<=ny and ny<= board and board_list[nx][ny] != 2:
+            snake.append((nx,ny))   #맵의 끝이 아니고 뱀 몸통이 아니라면 몸을 늘려 1칸 전진
+            if board_list[nx][ny] == 1:     #전진한 칸이 사과라면
+                board_list[nx][ny] = 2      #몸길이를 1칸 늘림
+            else:
+                board_list[nx][ny] = 2
+                px,py = snake.popleft()
+                board_list[px][py] = 0
+            x,y = nx,ny            
+            time+=1
+        else:
+            time+=1
+            break
+print(time)
+            
